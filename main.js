ee27e3d23a0b8d1c3007adccff672e71175f742b
UncaughtReference <uncaughtreferenceerror64@gmail.com>
2026-03-14 13:40:32 +0100

    a

--- main.js
+++ main.js
@@ -1,65 +1,52 @@
-function toHex2(buffer) {
-  return Array.prototype.map.call(buffer, x => ('00' + x.toString(16)).slice(-2)).join('');
+function intToHex(d) {
+      let number = (+d).toString(16).toUpperCase()
+      if( (number.length % 2) > 0 ) { number= "0" + number }
+      return number
 }
 
 function ramAddressChange(value) {
-    // Convert string to hexadecimal
-    var combinedValue = 0;
-    var convertedValue = 0;
-    value = value.toUpperCase();
-    for (var i = 0; i < value.length; i++) {
-        switch (value.charAt(i)) {
-            case "0":
-                convertedValue = 0;
-                break;
-            case "1":
-                convertedValue = 1;
-                break;
-            case "2":
-                convertedValue = 2;
-                break;
-            case "3":
-                convertedValue = 3;
-                break;
-            case "4":
-                convertedValue = 4;
-                break;
-            case "5":
-                convertedValue = 5;
-                break;
-            case "6":
-                convertedValue = 6;
-                break;
-            case "7":
-                convertedValue = 7;
-                break;
-            case "8":
-                convertedValue = 8;
-                break;
-            case "9":
-                convertedValue = 9;
-                break;
-            case "A":
-                convertedValue = 10;
-                break;
-            case "B":
-                convertedValue = 11;
-                break;
-            case "C":
-                convertedValue = 12;
-                break;
-            case "D":
-                convertedValue = 13;
-                break;
-            case "E":
-                convertedValue = 14;
-                break;
-            case "F":
-                convertedValue = 15;
-                break;
-        }
+    var invalid = false
+    var hexified = parseInt(value, 16);
+    var index = 0;
+    if (hexified<0x8016E000) {
+        invalid = true
     }
-    //hexified = toHex2(hexified);
-    document.getElementById("orga-display").innerHTML = combinedValue;
-    console.log(combinedValue)
+    else if (hexified<0x80246000) {
+        var final = ".orga 0x"+intToHex(hexified - 0x7FF4FB40);
+        index = 1
+    }
+    else if (hexified<0x80377800) {
+        var final = ".orga 0x"+intToHex(hexified - 0x80245000);
+        index = 2;
+    }
+    else if (hexified<0x80385F88){
+        var final = ".orga 0x"+intToHex(hexified - 0x80283280);
+        index = 3;
+    }
+    if (invalid == false) {
+        document.getElementById("orga-display").innerHTML = final;
+    }
+    else {
+        document.getElementById("orga-display").innerHTML = "Invalid input!";
+    }
+    if (index==0) {
+        document.getElementById("rom-block-1").style.borderColor = "#d9a3ff";
+        document.getElementById("rom-block-2").style.borderColor = "#d9a3ff";
+        document.getElementById("rom-block-3").style.borderColor = "#d9a3ff";
+    }
+    else if (index==1) {
+        document.getElementById("rom-block-1").style.borderColor = "#00b83a";
+        document.getElementById("rom-block-2").style.borderColor = "#d9a3ff";
+        document.getElementById("rom-block-3").style.borderColor = "#d9a3ff";
+    }
+    else if (index==2) {
+        document.getElementById("rom-block-1").style.borderColor = "#d9a3ff";
+        document.getElementById("rom-block-2").style.borderColor = "#00b83a";
+        document.getElementById("rom-block-3").style.borderColor = "#d9a3ff";
+    }
+    else if (index==3) {
+        document.getElementById("rom-block-1").style.borderColor = "#d9a3ff";
+        document.getElementById("rom-block-2").style.borderColor = "#d9a3ff";
+        document.getElementById("rom-block-3").style.borderColor = "#00b83a";
+    }
 }
